1783430fcf09f5079acbfda27a857ac724091f21
tomczk <tomasztaras@outlook.com>
2018-12-01 00:04:44 +0100

    change implementation of Heaps to be more OO and remove code duplication

--- ds/heaps.py
+++ ds/heaps.py
@@ -2,131 +2,125 @@
 from math import floor
 
 
-class MinHeap:
+class Heap:
+    """Base class for implementing Heaps"""
     def __init__(self):
-        self.__heap = []
+        self.heap = []
 
-    def __str__(self):
-        return "MinHeap - array representation: " + self.__heap.__str__()
+    def _up_heap(self, index: int):
+        # To be overridden
+        pass
 
-    def __up_heap(self, index: int):
-        parent_index = floor((index - 1) / 2)
-        parent_value = self.__heap[parent_index]
-        child_value = self.__heap[index]
-        if index == 0 or parent_value < child_value:
+    def _down_heap(self, index: int = 0):
+        # To be overridden
+        pass
+
+    def _first_value(self):
+        """Function returns first value from heap without modifying the heap. Returns None if heap is empty."""
+        try:
+            temp = self.heap[0]
+        except IndexError:
             return None
-        else:
-            temp = parent_value
-            self.__heap[parent_index] = child_value
-            self.__heap[index] = temp
-            return self.__up_heap(parent_index)
-
-    def __down_heap(self, index: int = 0):
-        left = 2 * index + 1
-        right = 2 * index + 2
-        largest = index
-
-        if left < len(self.__heap) and self.__heap[left] < self.__heap[largest]:
-            largest = left
-
-        if right < len(self.__heap) and self.__heap[right] < self.__heap[largest]:
-            largest = right
-
-        if largest != index:
-            temp = self.__heap[index]
-            self.__heap[index] = self.__heap[largest]
-            self.__heap[largest] = temp
-            return self.__down_heap(largest)
-        else:
-            return None
+        return temp
 
     def insert(self, value):
         """Function inserts given value into heap and returns inserted value."""
-        self.__heap.append(value)
-        self.__up_heap(len(self.__heap)-1)
+        self.heap.append(value)
+        self._up_heap(len(self.heap) - 1)
         return value
 
     def extract(self):
         """Function extracts min value from heap, removes it from heap and rebuilds the heap.
         Returns None if heap is empty."""
-        minval = self.min_value()
-        if minval:
-            if len(self.__heap) == 1:
-                self.__heap.pop()
+        first_value = self._first_value()
+        if first_value is not None:
+            if len(self.heap) == 1:
+                self.heap.pop()
             else:
-                self.__heap[0] = self.__heap.pop()
-                self.__down_heap()
-        return minval
-
-    def min_value(self):
-        """Function returns min value from heap without modifying the heap. Returns None if heap is empty."""
-        try:
-            temp = self.__heap[0]
-        except IndexError:
-            return None
-        return temp
+                self.heap[0] = self.heap.pop()
+                self._down_heap()
+        return first_value
 
 
-class MaxHeap:
-    def __init__(self):
-        self.__heap = []
+class MinHeap(Heap):
+    """Min Heap"""
 
     def __str__(self):
-        return "MaxHeap - array representation: " + self.__heap.__str__()
+        return "MinHeap - array representation: " + self.heap.__str__()
 
-    def __up_heap(self, index: int):
+    def _up_heap(self, index: int):
         parent_index = floor((index - 1) / 2)
-        parent_value = self.__heap[parent_index]
-        child_value = self.__heap[index]
-        if index == 0 or parent_value > child_value:
+        parent_value = self.heap[parent_index]
+        child_value = self.heap[index]
+        if index == 0 or parent_value < child_value:
             return None
         else:
             temp = parent_value
-            self.__heap[parent_index] = child_value
-            self.__heap[index] = temp
-            return self.__up_heap(parent_index)
+            self.heap[parent_index] = child_value
+            self.heap[index] = temp
+            return self._up_heap(parent_index)
 
-    def __down_heap(self, index: int = 0):
+    def _down_heap(self, index: int = 0):
         left = 2 * index + 1
         right = 2 * index + 2
         largest = index
 
-        if left < len(self.__heap) and self.__heap[left] > self.__heap[largest]:
+        if left < len(self.heap) and self.heap[left] < self.heap[largest]:
             largest = left
 
-        if right < len(self.__heap) and self.__heap[right] > self.__heap[largest]:
+        if right < len(self.heap) and self.heap[right] < self.heap[largest]:
             largest = right
 
         if largest != index:
-            temp = self.__heap[index]
-            self.__heap[index] = self.__heap[largest]
-            self.__heap[largest] = temp
-            return self.__down_heap(largest)
+            temp = self.heap[index]
+            self.heap[index] = self.heap[largest]
+            self.heap[largest] = temp
+            return self._down_heap(largest)
         else:
             return None
 
-    def insert(self, value):
-        """Function inserts given value into heap and returns inserted value."""
-        self.__heap.append(value)
-        self.__up_heap(len(self.__heap)-1)
-        return value
+    def min_value(self):
+        """Function returns min value from heap without modifying the heap. Returns None if heap is empty."""
+        return self._first_value()
 
-    def extract(self):
-        """Function extracts max value from heap, removes it from heap and rebuilds the heap.
-        Returns None if heap is empty."""
-        maxval = self.max_value()
-        if maxval:
-            if len(self.__heap) == 1:
-                self.__heap.pop()
-            else:
-                self.__heap[0] = self.__heap.pop()
-                self.__down_heap()
-        return maxval
+
+class MaxHeap(Heap):
+    """Max Heap"""
+
+    def __str__(self):
+        return "MaxHeap - array representation: " + self.heap.__str__()
+
+    def _up_heap(self, index: int):
+        parent_index = floor((index - 1) / 2)
+        parent_value = self.heap[parent_index]
+        child_value = self.heap[index]
+        if index == 0 or parent_value > child_value:
+            return None
+        else:
+            temp = parent_value
+            self.heap[parent_index] = child_value
+            self.heap[index] = temp
+            return self._up_heap(parent_index)
+
+    def _down_heap(self, index: int = 0):
+        left = 2 * index + 1
+        right = 2 * index + 2
+        largest = index
+
+        if left < len(self.heap) and self.heap[left] > self.heap[largest]:
+            largest = left
+
+        if right < len(self.heap) and self.heap[right] > self.heap[largest]:
+            largest = right
+
+        if largest != index:
+            temp = self.heap[index]
+            self.heap[index] = self.heap[largest]
+            self.heap[largest] = temp
+            return self._down_heap(largest)
+        else:
+            return None
 
     def max_value(self):
         """Function returns max value from heap without modifying the heap. Returns None if heap is empty."""
-        try:
-            temp = self.__heap[0]
-        except IndexError:
-            return None
-        return temp
+        return self._first_value()
