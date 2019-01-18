f9cff8c230b894a857c56cbc28ec53fd2f88de26
tomczk <tomasztaras@outlook.com>
2019-01-18 22:38:48 +0100

    depth first traversal

--- ds/binarytree.py
+++ ds/binarytree.py
@@ -1,6 +1,3 @@
-"""Sample implementation of a binary tree."""
-
-
 class Node:
     def __init__(self, value):
         self.value = value
@@ -11,3 +8,44 @@ class Node:
 class BinaryTree:
     def __init__(self, value):
         self.root = Node(value)
+
+    def pre_order_depth_first_traversal(self) -> list:
+        return self._pre_order_depth_first_traversal_recursive_helper_function(self.root, [])
+
+    def _pre_order_depth_first_traversal_recursive_helper_function(self, node: Node, values: list) -> list:
+        if node is None:
+            return values
+        values.append(node.value)
+        self._pre_order_depth_first_traversal_recursive_helper_function(node.left, values)
+        self._pre_order_depth_first_traversal_recursive_helper_function(node.right, values)
+        return values
+
+    def in_order_depth_first_traversal(self) -> list:
+        return self._in_order_depth_first_traversal_recursive_helper_function(self.root, [])
+
+    def _in_order_depth_first_traversal_recursive_helper_function(self, node: Node, values: list) -> list:
+        if node is None:
+            return values
+        self._in_order_depth_first_traversal_recursive_helper_function(node.left, values)
+        values.append(node.value)
+        self._in_order_depth_first_traversal_recursive_helper_function(node.right, values)
+        return values
+
+    def out_order_depth_first_traversal(self):
+        return self._out_order_depth_first_traversal_recursive_helper_function(self.root, [])
+
+    def _out_order_depth_first_traversal_recursive_helper_function(self, node: Node, values: list):
+        if node is None:
+            return values
+        self._out_order_depth_first_traversal_recursive_helper_function(node.left, values)
+        self._out_order_depth_first_traversal_recursive_helper_function(node.right, values)
+        values.append(node.value)
+        return values
+
+    def test(self, node: Node):
+        yield node.value
+        self.test(node.left)
+        self.test(node.right)
+
+    def breadth_first_traversal(self):
+        pass
